0882ee19afc03ada16ba52e95e865288df7a493a
Ubaidullah Hasan <ubaidullhasan2023@gmail.com>
2026-06-01 23:43:18 +0600

    html to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import "./App.css";
+import BackgroundPattern from "./components/BackgroundPattern";
+import Hero from "./components/Hero";
+import Nav from "./components/Nav";
+import VerticalLine from "./components/VerticalLine";
 
 function App() {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="w-full min-h-screen relative bg-[#F7F5F3] overflow-x-hidden flex flex-col justify-start items-center">
+      <div className="relative flex flex-col justify-start items-center w-full">
+        {/*  Main container with proper margins  */}
+        <div className="w-full max-w-none px-4 sm:px-6 md:px-8 lg:px-0 lg:max-w-[1060px] lg:w-[1060px] relative flex flex-col justify-start items-start min-h-screen">
+          <VerticalLine />
+          <Nav />
+          {/* <div className="self-stretch pt-[9px] overflow-hidden border-b border-[rgba(55,50,47,0.06)] flex flex-col justify-center items-center gap-4 sm:gap-6 md:gap-8 lg:gap-[66px] relative z-10 "> */}
+
+          {/* </div> */}
+
+          <div className="pt-16 sm:pt-20 md:pt-24 lg:pt-[216px] pb-8 sm:pb-12 md:pb-16 flex flex-col justify-start items-center px-2 sm:px-4 md:px-8 lg:px-0 w-full sm:pl-0 sm:pr-0 pl-0 pr-0">
+            <Hero />
+            <BackgroundPattern />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
